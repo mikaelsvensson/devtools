16d211e1d1c78eba870cc20659d342ea0be61c38
Mikael Svensson <mikael.svensson@sigma.se>
2012-06-14 10:56:36 +0200

    XmlDoclet and XmlAndStandardDoclet: first (second?) commit of something that actually does some decent work.

---
+++ src/main/java/info/mikaelsvensson/doclet/AbstractDoclet.java
@@ -0,0 +1,55 @@
+package info.mikaelsvensson.doclet;
+
+import com.sun.javadoc.LanguageVersion;
+import com.sun.javadoc.RootDoc;
+
+public class AbstractDoclet {
+
+    public RootDoc getRoot() {
+        return root;
+    }
+
+    private RootDoc root;
+
+    protected AbstractDoclet(RootDoc root) {
+        this.root = root;
+    }
+
+    protected String readOption(String optionName) {
+        return readOption(optionName, root);
+    }
+
+    protected static String readOption(String optionName, RootDoc root) {
+        String[][] options = root.options();
+        for (String[] opt : options) {
+            if (opt[0].equals(optionName)) {
+                return opt[1];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Indicate that this doclet supports the 1.5 language features.
+     * <p/>
+     * Credits to http://stackoverflow.com/questions/5731619/doclet-get-generics-of-a-list
+     *
+     * @return JAVA_1_5, indicating that the new features are supported.
+     */
+    public static LanguageVersion languageVersion() {
+        return LanguageVersion.JAVA_1_5;
+    }
+
+    protected void printNotice(String message) {
+        root.printNotice(message);
+    }
+
+    protected void printError(Exception e) {
+        root.printError(e.getMessage());
+    }
+
+    protected void printWarning(Exception e) {
+        root.printWarning(e.getMessage());
+    }
+
+}
