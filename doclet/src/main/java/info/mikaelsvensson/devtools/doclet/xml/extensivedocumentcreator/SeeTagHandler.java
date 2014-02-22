ad662914be583a5c76e879d9e3083e3fe807e9c6
Mikael Svensson <github@mikaelsvensson.info>
2014-02-22 10:42:31 +0100

    Renamed doctools to devtools (renaming folders changes)

---
+++ doclet/src/main/java/info/mikaelsvensson/devtools/doclet/xml/extensivedocumentcreator/SeeTagHandler.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2012, Mikael Svensson
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of the
+ *       contributors of this software may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL MIKAEL SVENSSON BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package info.mikaelsvensson.devtools.doclet.xml.extensivedocumentcreator;
+
+import com.sun.javadoc.SeeTag;
+import info.mikaelsvensson.devtools.doclet.shared.ElementWrapper;
+
+class SeeTagHandler extends TagHandler<SeeTag> {
+// --------------------------- CONSTRUCTORS ---------------------------
+
+    SeeTagHandler(final Dispatcher dispatcher) {
+        super(SeeTag.class, dispatcher);
+    }
+
+// -------------------------- OTHER METHODS --------------------------
+
+    @Override
+    void handleImpl(final ElementWrapper el, final SeeTag doc) throws JavadocItemHandlerException{
+        super.handleImpl(el, doc);
+
+        el.setAttributes(
+                "label", doc.label(),
+                "referenced-class", doc.referencedClassName(),
+                "referenced-member", doc.referencedMemberName());
+
+        if (doc.referencedPackage() != null) {
+            el.setAttribute("referenced-package", String.valueOf(doc.referencedPackage()));
+        }
+    }
+}
--- doclet/src/test/java/info/mikaelsvensson/doctools/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package comment
- */
-package info.mikaelsvensson.devtools;
