e581e0ed06763bcc9e501f8943135c9546583cf2
Mikael Svensson <github@mikaelsvensson.info>
2014-03-01 22:38:58 +0100

    Ignoring test with (incorrect) absolute paths.

--- common/src/test/java/info/mikaelsvensson/devtools/sitesearch/PathUtilsTest.java
+++ common/src/test/java/info/mikaelsvensson/devtools/sitesearch/PathUtilsTest.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2014 Mikael Svensson
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package info.mikaelsvensson.devtools.sitesearch;
 
 import info.mikaelsvensson.devtools.common.PathUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,6 +30,7 @@ import static org.junit.Assert.assertTrue;
 public class PathUtilsTest {
 
     @Test
+    @Ignore
     public void testGetRelativePath() throws Exception {
         String[] CHAIN_DOCLETINVOKER_JAVA = /*                      */ {"D:", "Dokument", "Utveckling", "devtools", "doclet", "src", "main", "java", "info", "mikaelsvensson", "devtools", "doclet", "chain", "DocletInvoker.java"};
         String[] CHAIN_DOCROOTWRAPPER_JAVA = /*                     */ {"D:", "Dokument", "Utveckling", "devtools", "doclet", "src", "main", "java", "info", "mikaelsvensson", "devtools", "doclet", "chain", "DocRootWrapper.java"};
